8a19cff0974ca5ccd638e07e31858d78c531ff4d
Sebastnt <sebastnt.p@hotmail.com>
2023-01-27 02:39:34 +0000

    traffic light project functional

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-
+import { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [change, setChange] = useState("");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container m-5">
+			<div className="traffic mx-auto text-center bg-dark"></div>
+			<div className="main-box mx-auto text-center bg-dark rounded-5 p-1">
+				<div className={`color bg-danger mt-2 ${(change === "red" ? "iluminado" : "")}`}  onClick={() => setChange("red")}></div>
+				<div className={`color bg-warning ${(change === "yellow" ? "iluminado" : "")}`} onClick={() => setChange("yellow")} ></div>
+				<div className={`color bg-success ${(change === "green" ? "iluminado" : "")}`} onClick={() => setChange("green")} ></div>
+			</div>
 		</div>
 	);
 };
